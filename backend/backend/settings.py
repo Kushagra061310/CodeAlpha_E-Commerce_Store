b783645ef9120a2547fe04ee651fb4cfba3ec6bb
Kushagra Goyal <gkushagra905@gmail.com>
2026-06-20 21:34:28 +0530

    Updated API URL to Render production

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -120,5 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+CORS_ALLOWED_ORIGINS = [
+    "https://https://vercel.com/kushagra061310s-projects/code-alpha-e-commerce-store.vercel.app", 
+]
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
